c1bfd849890ea9c7016761dd87d7b811abb28a1f
ARTEM211222 <artemsadivskij@gmail.com>
2024-08-03 17:31:02 +0300

    commit

---
+++ memorycard-main/mani.py
@@ -0,0 +1,21 @@
+from PyQt5.QtWidgets import QWidget
+from card import mani_line
+from app import App 
+
+
+win_card = QWidget()
+win_card.resize(600,500)
+win_card.setWindowTitle("Memory Card")
+win_card.move(0,0)
+
+
+win_card.setLayout(mani_line)
+
+win_card.show()
+App.exec_()
+
+
+
+
+
+
